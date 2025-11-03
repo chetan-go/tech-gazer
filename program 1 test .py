f98cc86e80554890b34dd44d93c8dd6f9887412e
chetan-go <141745140+chetan-go@users.noreply.github.com>
2025-11-03 15:57:52 +0000

    git commit -m "added first heading to program 1 test.py" git push

--- program 1 test .py
+++ program 1 test .py
@@ -1,1 +1,9 @@
+import shutil
 
+def print_centered(text):
+    width = shutil.get_terminal_size((80, 20)).columns
+    for line in str(text).splitlines():
+        print(line.center(width))
+
+if __name__ == "__main__":
+    print_centered("Welcome to Tech gazer!")
