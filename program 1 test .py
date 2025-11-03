5a0549186a5b92f9f8404c9c15ad2eb5088460c0
AADITHYA84 <aadithya0809@gmail.com>
2025-11-03 21:36:21 +0530

    git.push

--- program 1 test .py
+++ program 1 test .py
@@ -7,3 +7,12 @@ def print_centered(text):
 
 if __name__ == "__main__":
     print_centered("Welcome to Tech gazer!")
+
+a=input("Enter yes if u want to see all products available in our stores, No to continue and select your order:")
+if a.lower() == "yes":
+    print("Here are all the products available in our stores:")
+    print("Product 1: Tech Gadget A")
+    print("Product 2: Tech Gadget B")
+    print("Product 3: Tech Gadget C")
+else:
+    print("Proceeding to order selection...")
