0dfcfd5208fd49419ed66ce8ff3430a9f67acffe
Nihaan <nnihaan2009@gmail.com>
2026-01-07 16:36:27 +0000

    git.push

--- program 1 test .py
+++ program 1 test .py
@@ -3,7 +3,7 @@ import shutil
 def print_centered(text):
     width = shutil.get_terminal_size((80, 20)).columns
     for line in str(text).splitlines():
-        print(line.center(width))
+        print(line.center(width
 
 if __name__ == "__main__":
     print_centered("================================")
